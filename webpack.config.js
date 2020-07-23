6dd672d7d05a9f1a8fd34cce50e9850955392971
fangyuemeng <yuemeng.fang@macrowing.com>
2020-07-23 17:51:03 +0800

    调整webpack打包后的img路径显示问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { resolve } = require("path");
@@ -26,51 +26,68 @@ module.exports = {
             filename: "html/header.html",
             chunkFilename: "[id].js",
         }),
-        
+
         new MiniCssExtractPlugin({
             filename: "css/[name].[hash].css",
             chunkFilename: "[id].css",
         })
     ],
     module: {
-        rules:[
-          {
-            test:/\.css$/,
-            use:[{
-                loader: MiniCssExtractPlugin.loader,
-            }, 'style-loader','css-loader'] // 从右向左解析原则
-          },
-          {
-            test:/\.less$/,
-            use:[{
-                loader: MiniCssExtractPlugin.loader,
-            },{
-                loader: 'css-loader',
-            },{
-                loader:'postcss-loader',
-                options:{
-                    plugins:[require('autoprefixer')]
-                }
-            },{
-                loader: 'less-loader',
-            }]
-          },
-          {
-            test:/\.js$/,
-            exclude: "/node_modules/",
-            include:resolve(__dirname, "src"),
-            use:{
-              loader:'babel-loader',
-              options:{
-                presets:['@babel/preset-env']
-              }
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                }, 'style-loader', 'css-loader'] // 从右向左解析原则
             },
-            exclude:/node_modules/
-          },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                }, {
+                    loader: 'css-loader',
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [require('autoprefixer')]
+                    }
+                }, {
+                    loader: 'less-loader',
+                }]
+            },
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                include: resolve(__dirname, "src"),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(htm|html)$/i,
+                use: ['html-withimg-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader:'file-loader',
+                    options: {
+                        outputPath: './images', //打包后的图片放到img文件夹下,
+                        publicPath: '../images',
+                        limit: 10000,
+                        esModule: false,
+                        name: "[name].[ext]"
+                    }
+                }]
+            },
         ]
     },
     devServer: {
-        contentBase: "./dist/",
+        contentBase: "./dist",
         port: "8080",
         inline: true,
         openPage: "html/index.html"
